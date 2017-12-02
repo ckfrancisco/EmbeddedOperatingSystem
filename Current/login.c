9321fea4a8eb117ddf4be8b691a5c26905cfbc4d
ckfrancisco <chr.francisco@gmail.com>
2017-12-02 10:25:04 -0800

    Started login

--- Current/login.c
+++ Current/login.c
@@ -1,32 +1,106 @@
 /****************** Algorithm of login *******************/
-// login.c : Upon entry, argv[0]=login, argv[1]=/dev/ttyX #include "ucode.c"
-int in, out, err; char name[128],password[128]
-main(int argc, char *argv[])
-{
-    close(0); close(1);
-    open()
-(3) settty(argv[1]); // set tty name string in PROC.tty
-(4) open /etc/passwd file for READ;
+// login.c : Upon entry, argv[0]=login, argv[1]=/dev/5X #include "ucode.c"
 
-    while(1){
-(5)     printf("login:");
-        gets(name);
-        printf("password:");
-        gets(password);
-        for each line in /etc/passwd file do{
-            tokenize user account line;
-(6)         if (user has a valid account){
-(7)             change uid, gid to users uid, gid;  // chuid()
-                change cwd to users home DIR        // chdir()
-                close opened /etc/passwd file       // close()
-                exec to program in user account     // exec()
-            }
-        }
-(8)     printf("login failed, try again\n");
+#include "ucode.c"
+
+int tokenize(int fd, char *buf, char token, int position)
+{
+    int nbytes = 0;
+
+    while(read(fd, buf, 1) > 0 && *buf != token)
+        nbytes++;
+
+    lseek(fd, position, 0);
+
+    read(fd, buf, nbytes);
+    buf[nbytes] = 0;
+
+    return nbytes;
+}
+
+int myatoi(char *buf)
+{
+    int result = 0;
+
+    while(*buf)
+    {
+        result += (*buf - '0');
+
+        if(*(buf++))
+            result *= 10;
     }
 }
 
-  
+int mystrcmp(char *s1, char *s2)
+{
+    while(*(s1++) == *(s2++) && *s1 && *s2);
 
+    if(!(*s1 && *s2))
+        return 1;
+    return 0;
+}
 
+main(int argc, char *argv[])
+{
+    int in, out, err, passwd;
+    char name[128], password[128];
 
+    int line, position;
+    char buf1[256], buf2[256];
+    int uid, gid;
+
+    int test = 0;
+    
+
+    close(0); close(1);
+    in  = open(argv[1], O_RDONLY); 
+    out = open(argv[1], O_WRONLY);
+    err = open(argv[1], O_WRONLY);
+
+    fixtty(argv[1]);                    // set tty name string in PROC.tty
+
+    passwd = open("/etc/passwd", O_RDONLY);  // open /etc/passwd file for READ;
+
+    while(1){
+        printf("login:");
+        gets(name);
+        printf("password:");
+        gets(password);
+
+        line = position = 0;
+
+        while(tokenize(passwd, buf1, '\r', position))
+        {
+            lseek(passwd, position, 0);
+            printf("line=%d position=%d test=%d buf=%s\n\r", line, position, test++, buf1);
+
+            line = tokenize(passwd, buf1, '\n', position);
+            printf("line=%d position=%d test=%d buf=%s\n\r", line, position, test++, buf1);
+
+            printf("%d %s\n\r", test++, buf1);
+            position = line;
+            position += tokenize(passwd, buf1, ':', position);
+            position += tokenize(passwd, buf2, ':', position);
+
+            if(mystrcmp(name, buf1) && mystrcmp(password, buf2))
+            {
+                position += tokenize(passwd, buf1, ':', position);
+                position += tokenize(passwd, buf2, ':', position);
+                uid = myatoi(buf1); gid = myatoi(buf2);
+                chuid(uid, gid);
+
+                position += tokenize(passwd, buf1, ':', position);
+                chdir(buf1);
+
+                close(passwd);
+
+                position += tokenize(passwd, buf1, '\n', position);
+                exec(buf1);
+
+                return;
+            }
+        }
+
+        printf("login failed, try again\n");
+    }
+}
