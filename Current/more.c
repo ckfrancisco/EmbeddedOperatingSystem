269ab94cc20f68cff82a5cdef8f1e04832dded60
ckfrancisco <chr.francisco@gmail.com>
2017-12-03 11:44:42 -0800

    Show help during more without parameters

--- Current/more.c
+++ Current/more.c
@@ -5,29 +5,56 @@ main(int argc, char *argv[])
     int fd, ncount, maxcount;
     char c;
 
-    ncount = 0;
-    maxcount = 20;
-
-    fd = open(argv[1], O_RDONLY);
-
-    while(read(fd, &c, 1) > 0)
+    if(argc < 2)
     {
-        mputc(c);
+        printf("Usage:\n"
+                "   more [options] <file>...\n"
 
-        if(c == '\n')
-            ncount++;
+                "A file perusal filter for CRT viewing.\n"
 
-        if(ncount == maxcount)
+                "Options:\n"
+                "   -d          display help instead of ringing bell\n"
+                "   -f          count logical rather than screen lines\n"
+                "   -l          suppress pause after form feed\n"
+                "   -c          do not scroll, display text and clean line ends\n"
+                "   -p          do not scroll, clean screen and display text\n"
+                "   -s          squeeze multiple blank lines into one\n"
+                "   -u          suppress underlining\n"
+                "   -<number>   the number of lines per screenful\n"
+                "   +<number>   display file beginning from line number\n"
+                "   +/<string>  display file beginning from search string match\n"
+                "   -V          display version information and exit\n"
+
+                "For more details see more(1).\n\r");
+    }
+    else
+    {
+        ncount = 0;
+        maxcount = 20;
+        
+        fd = open(argv[1], O_RDONLY);
+
+        while(read(fd, &c, 1) > 0)
         {
-            ncount = 0;
-            c = getc();
+            mputc(c);
 
-            if(c == ' ')
-                maxcount = 20;
-            else
-                maxcount = 1;
+            if(c == '\n')
+                ncount++;
+
+            if(ncount == maxcount)
+            {
+                ncount = 0;
+                c = getc();
+
+                if(c == ' ')
+                    maxcount = 20;
+                else
+                    maxcount = 1;
+            }
         }
     }
+
+    
 
     close(fd);
 }
