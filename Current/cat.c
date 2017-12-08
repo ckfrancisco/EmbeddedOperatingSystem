7bf8368afa9448ce25b19f99a9d6b1243ff4275d
ckfrancisco <chr.francisco@gmail.com>
2017-12-08 15:31:08 -0800

    Cat prints to screen and file in special case

--- Current/cat.c
+++ Current/cat.c
@@ -2,9 +2,9 @@
 
 main(int argc, char *argv[])
 {
-    int fd;
+    int fd, out;
     char tty[16], c;
-    STAT ttystat, instat;
+    STAT ttystat, instat, outstat;
 
     if(argc > 1)
     {
@@ -12,7 +12,7 @@ main(int argc, char *argv[])
         if(fd < 0)
         {
             printf("Error: %s does not exist\n\r", argv[2]);
-            exit(0);
+            exit(1);
         }
 
         dup2(fd, 0); 
@@ -20,15 +20,29 @@ main(int argc, char *argv[])
 
     gettty(tty);
     stat(tty, &ttystat);
-    fstat(1, &instat);
+    fstat(0, &instat);
+    fstat(1, &outstat);
+    out = open(tty, O_WRONLY);
         
     if(ttystat.st_dev == instat.st_dev && ttystat.st_ino == instat.st_ino)
         while(read(0, &c, 1))
+        {
             mputc(c);
+            
+            if(ttystat.st_dev != outstat.st_dev || ttystat.st_ino != outstat.st_ino)
+            {
+                write(out, &c, 1);
+                if(c == '\r')
+                {
+                    c = '\n';
+                    write(out, &c, 1);
+                }
+            }
+        }
 
     else
         while(read(0, &c, 1))
             write(1, &c, 1);
 
-    exit(1);
+    exit(0);
 }
